ccf5b643bfe1f6532a4b176e484fb90c1e278828
gogakoreli <gogakoreli@gmail.com>
2018-08-19 15:37:12 +0400

    fix initialization; devexperience

--- goald-api/lib/app.ts
+++ goald-api/lib/app.ts
@@ -3,11 +3,13 @@ import * as express from 'express';
 import * as mongoose from 'mongoose';
 import { Routes } from './routes/crmRoutes';
 
+// "start": "npm run build:live",
+
 class App {
   public app: express.Application;
   public routePrv: Routes = new Routes();
 
-  public mongoUrl: string = 'mongodb://localhost/CRMdb';
+  public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';
 
   constructor() {
     this.app = express();
@@ -23,7 +25,10 @@ class App {
 
   private mongoSetup(): void {
     mongoose.Promise = global.Promise;
-    mongoose.connect(this.mongoUrl);
+    mongoose.connect(
+      this.mongoUrl,
+      { useNewUrlParser: true },
+    );
   }
 }
 
